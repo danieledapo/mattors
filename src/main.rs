f3b6ad055015873cca523c0a597c749a6085f61c
d-dorazio <daniele.dorazio@adroll.com>
2017-10-12 21:48:48 +0200

    cleanup

--- src/main.rs
+++ src/main.rs
@@ -10,8 +10,6 @@ use num::complex::Complex64;
 use matto::julia::{fractal_to_image, gen_fractal, Bound, FractalPoint};
 use matto::dragon;
 
-const STEP: f64 = 0.002;
-
 
 fn main() {
     julia_fractals();
@@ -21,35 +19,32 @@ fn main() {
 
 fn julia_fractals() {
     let manifest = vec![
-        (Bound::new(-3.0, -1.2), Bound::new(1.0, 1.2), STEP, None),
+        (Bound::new(-3.0, -1.2), Bound::new(1.0, 1.2), None),
         (
             Bound::new(-3.0, -1.2),
             Bound::new(2.0, 1.2),
-            STEP,
             Some(Complex64::new(-0.4, 0.6)),
         ),
         (
             Bound::new(-3.0, -1.2),
             Bound::new(2.0, 1.2),
-            STEP,
             Some(Complex64::new(-0.8, 0.156)),
         ),
         (
             Bound::new(-1.2, -1.2),
             Bound::new(1.2, 1.0),
-            STEP,
             Some(Complex64::new(0.285, 0.01)),
         ),
     ];
 
     for (i, row) in manifest.iter().enumerate() {
-        let (ref start, ref end, step, ref c) = *row;
+        let (ref start, ref end, ref c) = *row;
 
         let frac = {
             if let Some(c) = *c {
-                gen_fractal(start, end, step, |f| FractalPoint::julia(f, c))
+                gen_fractal(start, end, 0.002, |f| FractalPoint::julia(f, c))
             } else {
-                gen_fractal(start, end, step, FractalPoint::mandelbrot)
+                gen_fractal(start, end, 0.002, FractalPoint::mandelbrot)
             }
         };
 
