a3789f5bb48956cc983dbe366a019efdce8a826f
Daniele D'Orazio <d.dorazio96@gmail.com>
2019-08-20 13:08:46 +0200

    sketches/bw-rain: tuning

--- sketches/bw-rain.ts
+++ sketches/bw-rain.ts
@@ -14,11 +14,14 @@ export class BlackWhiteRain implements ISketch {
     public draw(p: p5) {
         p.background("white");
 
-        for (let x = 0; x < this.width; x += 5) {
-            let s = 5 + ((x / 50) * this.width) / this.height;
+        const nDiags = p.random(20, 200);
+        const horStep = p.random(2, 5);
+
+        for (let x = 0; x < this.width; x += horStep) {
+            let s = horStep + ((x / nDiags) * this.width) / this.height;
 
             for (let y = 0; y < this.height; y += s) {
-                const t = Math.max((y + s) / this.height, (x + 5) / this.width);
+                const t = Math.max((y + s) / this.height, (x + horStep) / this.width);
 
                 if (p.random() > t) {
                     p.line(x, y, x, y + s);
